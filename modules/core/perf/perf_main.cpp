5337ba148c64d491150a425adc67a798a6aa2e6f
Jakub-Golinowski <jakub.golinowski@gmail.com>
2018-06-23 19:16:13 +0200

    Conditionally include hpx_main.hpp to the core module tests Header hpx_main.hpp is conditionally included to both core/perf/perf_main.cpp and core/test/test_main.cpp

--- modules/core/perf/perf_main.cpp
+++ modules/core/perf/perf_main.cpp
@@ -4,5 +4,8 @@
 #  pragma warning(disable:4447) // Disable warning 'main' signature found without threading model
 # endif
 #endif
+#ifndef HPX_STARTSTOP
+    #include <hpx/hpx_main.hpp>
+#endif
 
 CV_PERF_TEST_MAIN(core)
